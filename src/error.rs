7581ad092c79c583328c43ddfee03291f0b191a5
belltoy <belltoy@gmail.com>
2020-06-14 18:16:37 +0800

    impl std::error::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -9,7 +9,6 @@ pub enum Error {
     JsonError(serde_json::Error),
     NsqError(NsqError),
     TlsError(native_tls::Error),
-    // #[cfg(future = "snappy")]
     SnapError(snap::Error),
     DeflateCompressError(flate2::CompressError),
     DeflateDecompressError(flate2::DecompressError),
@@ -37,6 +36,48 @@ impl NsqError {
         match self.code.as_str() {
             "E_FIN_FAILED" | "E_REQ_FAILED" | "E_TOUCH_FAILED" => false,
             _ => true,
+        }
+    }
+}
+
+impl std::error::Error for NsqError {}
+
+impl std::fmt::Display for NsqError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NsqError: {}, description: {}", self.code, self.description)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        use Error::*;
+        match self {
+            IoError(e) => Some(e),
+            Utf8Error(e) => Some(e),
+            JsonError(e) => Some(e),
+            NsqError(e) => Some(e),
+            TlsError(e) => Some(e),
+            SnapError(e) => Some(e),
+            DeflateCompressError(e) => Some(e),
+            DeflateDecompressError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Error::*;
+        match self {
+            IoError(e) => e.fmt(f),
+            Utf8Error(e) => e.fmt(f),
+            JsonError(e) => e.fmt(f),
+            NsqError(e) => e.fmt(f),
+            TlsError(e) => e.fmt(f),
+            SnapError(e) => e.fmt(f),
+            DeflateCompressError(e) => e.fmt(f),
+            DeflateDecompressError(e) => e.fmt(f),
+            Auth(e) => write!(f, "Auth Error: {}", e),
         }
     }
 }
